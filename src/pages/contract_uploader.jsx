649cb7ae3c07e12bda313dfef9e8041b8401b41f
ngjc-wm19@student.tarc.edu.my <ngjc-wm19@student.tarc.edu.my>
2022-11-16 16:07:30 +0800

    Setup Document Uploader

--- src/pages/contract_uploader.jsx
+++ src/pages/contract_uploader.jsx
@@ -1,20 +1,71 @@
 import logo from './../logo.svg';
-import React from "react";
+import './contract_uploader.css';
+import React, {useCallback}  from "react";
+import {useDropzone} from "react-dropzone";
 
-function ContractUploader() {
-    //special of jsx file is html code can be store in to var for processing
-    var file = <code >src/pages/contract_uploader.jsx</code>;
+function ContractUploader(props) {
+  const {
+    acceptedFiles,
+    fileRejections,
+    getRootProps,
+    getInputProps
+  } = useDropzone({
+    accept: {
+      'image/jpeg': [],
+      'image/png': []
+    }
+  });
 
-    return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-                Edit {file} and save to reload.
-            </p>
-          </header>
+  const acceptedFileItems = acceptedFiles.map(file => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+
+  const fileRejectionItems = fileRejections.map(({ file, errors }) => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+      <ul>
+        {errors.map(e => (
+          <li key={e.code}>{e.message}</li>
+        ))}
+      </ul>
+    </li>
+  ));
+
+  return (
+    <div className='uploader_container'>
+      <h5 className='container_title'>Upload Your Smart Contract</h5>
+      {/* uploader */}
+      <section className="container">
+        <div {...getRootProps({ className: 'dropzone' })}>
+          <input {...getInputProps()} />
+          <p>Drag 'n' drop some files here, or <span>click</span> to select files</p>
+          <em>(Only *.jpeg and *.png images will be accepted)</em>
         </div>
-    )
+        <aside>
+          <h4>Accepted files</h4>
+          <ul>{acceptedFileItems}</ul>
+          <h4>Rejected files</h4>
+          <ul>{fileRejectionItems}</ul>
+        </aside>
+      </section>
+    </div>
+  );
+
+    //special of jsx file is html code can be store in to var for processing
+    // var file = <code >src/pages/contract_uploader.jsx</code>;
+
+    // return (
+    //     <div className="App">
+    //       <header className="App-header">
+    //         <img src={logo} className="App-logo" alt="logo" />
+    //         <p>
+    //             Edit {file} and save to reload.
+    //         </p>
+    //       </header>
+    //     </div>
+    // )
 }
 
 export default ContractUploader;
