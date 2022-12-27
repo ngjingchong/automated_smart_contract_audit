f1244cb1ca2a0764f120bb7f037c04ddb9208368
ngjc-wm19@student.tarc.edu.my <ngjc-wm19@student.tarc.edu.my>
2022-12-28 01:43:30 +0800

    add auto increment on the progress bar

--- src/pages/contract_scan_progress_indicator.jsx
+++ src/pages/contract_scan_progress_indicator.jsx
@@ -1,8 +1,32 @@
 import './contract_scan_progress_indicator.scss';
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { CircularProgressbar } from 'react-circular-progressbar';
 
-const Scan = ({percentage}) => {
+const Scan = ({percent, setPercent}) => {
+  const [percentage, setPercentage] = useState(percent)
+
+  useEffect(() => {
+    if(percentage !== 100) {
+      const remaining_percentage = 100 - percentage
+      // console.log("remaining => " + remaining_percentage)
+
+      const new_percentage = Math.trunc((percentage + (remaining_percentage*0.3)))
+      if (new_percentage < 100) {
+        // console.log("new => " + new_percentage)
+        setTimeout(() => setPercentage(new_percentage), 1000);
+        if (new_percentage === 97){
+          setTimeout(() => setPercentage(100), 1000);
+        }
+      }  
+    } else {
+      setPercent(percentage)
+    }
+  },[percentage])
+
+  useEffect(() => {
+    document.getElementsByClassName("_3CDiP")[0].style.display = "none"
+  },[])
+
   return (
     <div className="progress_bar_container">
       <CircularProgressbar 
