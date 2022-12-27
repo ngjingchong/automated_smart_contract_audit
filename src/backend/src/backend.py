2c70e6a630dea6d6768aa621b8abf2a24951be8e
ngjc-wm19@student.tarc.edu.my <ngjc-wm19@student.tarc.edu.my>
2022-12-28 01:45:13 +0800

    move detector_dir to global and handle contract posted to be audit

--- src/backend/src/backend.py
+++ src/backend/src/backend.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-# from slither.detectors import all_detectors
+from slither.detectors import all_detectors
 import inspect
-# from slither.detectors.abstract_detector import DetectorClassification, AbstractDetector
+from slither.detectors.abstract_detector import DetectorClassification, AbstractDetector
 import json
 
 
@@ -11,11 +11,12 @@ cors = CORS(api)
 
 import sys
 import os
+
 # os.system('cmd /c "pip install -r requirements.txt"')
 
-# contracts = ["Vault.sol", "Reentrancy.sol"] #get name of contracts to be audited in this session
+# contracts = ["Reentrancy.sol"] #get name of contracts to be audited in this session
 # for c in contracts:
-  # os.system('cmd /c "slither ./contracts/' + c + ' --solc-remaps @openzeppelin=../../node_modules/@openzeppelin --json ./src/reports/report_'+ c +'.json"')
+#   os.system('cmd /c "slither ./contracts/' + c + ' --solc-remaps @openzeppelin=../../node_modules/@openzeppelin --detect abiencoderv2-array,array-by-reference,storage-array --json ./reports/report_'+ c +'.json"')
 
 
 # file_path = r'.\contracts\Reentrancy.sol'    
@@ -28,104 +29,50 @@ import os
 # except FileNotFoundError:
 #     print("Please check the path.")
 
-@api.route('/upload-contracts', methods=['GET', 'POST'])
+detectors_dictionary = {
+  "all": [],
+  "reentrancy": ["reentrancy-eth", "reentrancy-no-eth", "reentrancy-benign", "reentrancy-events", "reentrancy-unlimited-gas"],
+  # "obselete_elements":["constant-function-asm", "constant-function-state", "deprecated-standards", "pragma", "solc-version"],
+  # "access_control": ["protected-vars", "suicidal", "unprotected-upgrade", "variable-scope", "events-access", "events-maths"],
+  # "interface": ["domain-separator-collision", "erc20-interface", "erc721-interface", "erc20-indexed"],
+  # "missing_initialization": ["uninitialized-state", "uninitialized-storage", "uninitialized-local", "uninitialized-fptr-cst", "void-cst", "function-init-state"],
+  # "treacherous_elements_usage": ["backdoor", "shadowing-state", "controlled-delegatecall", "shadowing-abstract", "tx-origin", "shadowing-builtin", "shadowing-local", "assembly", "rtlo", "public-mappings-nested"],
+  # "erroneous_usage": ["weak-prng", "timestamp", "similar-names", "too-many-digits"],
+  # "redundancy": ["multiple-constructors", "name-reused", "write-after-write", "reused-constructor", "unused-return", "redundant-statements", "unimplemented-functions", "unused-state", "dead-code"],
+  "storage_manipulation": ["abiencoderv2-array", "array-by-reference", "storage-array"],
+  # "best_practices": ["incorrect-shift", "controlled-array-length", "unchecked-transfer", "incorrect-equality", "locked-ether", "mapping-deletion", "divide-before-multiply", "unchecked-lowlevel", "unchecked-send", "incorrect-modifier", "incorrect-unary", "missing-zero-check", "assert-state-change", "missing-inheritance", "naming-convention", "constable-states", "external-function"],
+  # "low_level": ["arbitrary-send-erc20", "arbitrary-send-erc20-permit", "arbitrary-send-eth", "low-level-calls"],
+  # "contraditions_affirmation": ["enum-conversion", "tautology", "boolean-cst", "boolean-equal"],
+  # "loops": ["delegatecall-loop", "msg-value-loop", "calls-loop", "costly-loop"]
+  "": [],
+}
+
+@api.route('/scan_vulnerabilities', methods=['GET', 'POST'])
 def contract_uploaded():
-  contracts = [{
-    "name": "Nagato",
-    "about" :"Hello! I'm a full stack developer that loves python and javascript"
-  }]
   if request.method == 'POST':
-    # contracts = json.loads(request.data)
-    # file = contracts['contracts'][0]
-    # try:
-    #   os.system('cmd /c "slither ' + file['path'] + ' --solc-remaps @openzeppelin=../../node_modules/@openzeppelin --json ./src/reports/report_testing.json"')
-    # except:
-    #   print("Oops!", sys.exc_info()[0], "occurred.")
-    # print()
-    files = request.form.to_dict()
-    
-    # Iterate over the files and save them
-    # print(files)
-    # print(files.values())
-    
-    return(request.form.values())
-    # for file in files:
-      # file.save(os.path.join('/uploads', file.filename))
-    temp = []
-    for value in files:
-    #   os.system('cmd /c "slither ' + value + ' --solc-remaps @openzeppelin=../../node_modules/@openzeppelin --json ./src/reports/report_testing.json"')
-      # temp.append({
-      #   "file": value
-      # })
-      print(value)
-      # print("\n\n\n")
-    # for file in files:
-      # Save the file to the desired location
-      # print(file[0])
-      # os.system('cmd /c "slither ' + file + ' --solc-remaps @openzeppelin=../../node_modules/@openzeppelin --json ./src/reports/report_testing.json"')
+    # detector selected by user
+    if(request.form.get("detector") != "all"):
+      detector_selected = request.form.get("detector")
+      detector_selected = ','.join(detectors_dictionary[detector_selected])
+      detector_selected = "--detect " + detector_selected
+    else:
+      detector_selected = ""
 
-
-    # print(data)
-    return json.loads(json.dumps(temp))
-    contracts.append(request.files)
-  return contracts
-
-@api.route('/scan', methods=['GET', 'POST'])
-def scan_contracts():
-  if request.method == 'POST':
-    try: 
-      detector_list = json.loads(request.data)
-      detectors_str = ','.join(detector_list['detectors'])
-
-      # os.system('cmd /c "slither ' + file_path + ' --solc-remaps @openzeppelin=../../node_modules/@openzeppelin --detect ' + detectors_str +' --json ./src/reports/report_testing.json"')
-
-      return detectors_str
-    except:
-        print("Oops!", sys.exc_info()[0], "occurred.")
-        print("Next entry.") 
-        print()
-  
-  # get detector chosen to be scan and perform scanning
-  # upon scanning completion, save the result as json file
-  return "hello"
+    # files passed in to be scanned
+    files = request.files
+    for f in files:
+      # print(f)    
+      # f.stream.seek(0)
+      files[f].save(os.path.join("./contracts", f))
+      os.system('cmd /c "slither ./contracts/'+ str(f) +' --solc-remaps @openzeppelin=../../node_modules/@openzeppelin ' + detector_selected + ' --json ./reports/report_'+ f +'.json"')
+  return "Success"
 
 @api.route('/detectors', methods=['GET'])
 def available_detectors():
-  """ To obtain all the published detectors in the library """
-  # detectors_ = dir(all_detectors)
-  # detectors = [d for d in dir(all_detectors) if d[0].isalpha()]
-  # print(len(*detectors))    #print length of the detectors
-  # print(*detectors, sep='\n') #print each item in detectors in new line
-
-  # detectors_ = [getattr(all_detectors, name) for name in dir(all_detectors)]
-  # detectors = [d for d in detectors_ if inspect.isclass(d) and issubclass(d, AbstractDetector)]
-  # for index, detector in enumerate(detectors):
-  #   confident_level = -1
-  #   impact_level = -1
-  #   print(str(index) + "Impact : " + detector.IMPACT.name + " > " + str(detector.IMPACT.value))
-  #   print(str(index) + "Confident : " + detector.CONFIDENCE.name + " > " + str(detector.CONFIDENCE.value))
-
-  # for dir in detectors_dictionary:
-  #   print(detectors_dictionary[dir])
-  
+  """ To obtain all the published detectors in the library """  
   detectors_ = [getattr(all_detectors, name) for name in dir(all_detectors)]
   detectors = [d for d in detectors_ if inspect.isclass(d) and issubclass(d, AbstractDetector)]
   detectors_json = []
-  detectors_dictionary = {
-    "reentrancy": ["reentrancy-eth", "reentrancy-no-eth", "reentrancy-benign", "reentrancy-events", "reentrancy-unlimited-gas"],
-    "obselete_elements":["constant-function-asm", "constant-function-state", "deprecated-standards", "pragma", "solc-version"],
-    "access_control": ["protected-vars", "suicidal", "unprotected-upgrade", "variable-scope", "events-access", "events-maths"],
-    "interface": ["domain-separator-collision", "erc20-interface", "erc721-interface", "erc20-indexed"],
-    "missing_initialization": ["uninitialized-state", "uninitialized-storage", "uninitialized-local", "uninitialized-fptr-cst", "void-cst", "function-init-state"],
-    "treacherous_elements_usage": ["backdoor", "shadowing-state", "controlled-delegatecall", "shadowing-abstract", "tx-origin", "shadowing-builtin", "shadowing-local", "assembly", "rtlo", "public-mappings-nested"],
-    "erroneous_usage": ["weak-prng", "timestamp", "similar-names", "too-many-digits"],
-    "redundancy": ["multiple-constructors", "name-reused", "write-after-write", "reused-constructor", "unused-return", "redundant-statements", "unimplemented-functions", "unused-state", "dead-code"],
-    "storage_manipulation": ["abiencoderv2-array", "array-by-reference", "storage-array"],
-    "best_practices": ["incorrect-shift", "controlled-array-length", "unchecked-transfer", "incorrect-equality", "locked-ether", "mapping-deletion", "divide-before-multiply", "unchecked-lowlevel", "unchecked-send", "incorrect-modifier", "incorrect-unary", "missing-zero-check", "assert-state-change", "missing-inheritance", "naming-convention", "constable-states", "external-function"],
-    "low_level": ["arbitrary-send-erc20", "arbitrary-send-erc20-permit", "arbitrary-send-eth", "low-level-calls"],
-    "contraditions_affirmation": ["enum-conversion", "tautology", "boolean-cst", "boolean-equal"],
-    "loops": ["delegatecall-loop", "msg-value-loop", "calls-loop", "costly-loop"]
-  }
 
   for d in detectors_dictionary:
     detector_info = [{
