21fae6c8f10e7da997345fa9138a6dc805fa0d98
ngjc-wm19@student.tarc.edu.my <ngjc-wm19@student.tarc.edu.my>
2022-12-28 01:44:32 +0800

    move detector_dir to global & handle contract posted

--- src/pages/audit_process.jsx
+++ src/pages/audit_process.jsx
@@ -15,9 +15,8 @@ import AuditReport from './contract_audit_report';
 function Audit_Process() {
   const [contracts, setContracts] = useState([])
   const [detectorSelected, setDetector] = useState()
-  var [percentage, setPercentage] = useState(66)
-  const prevContractsRev = useRef();
-  const isRendered = useRef(true);
+  const [percent, setPercent] = useState(0)
+  const prevContractsRev = useRef(contracts)
 
   const setContractsHandler = (new_contracts) => {
     const new_contracts_list = prevContractsRev.current.concat(new_contracts)
@@ -35,69 +34,45 @@ function Audit_Process() {
   }
 
   useEffect(() => {
+    document.getElementById("logoutAlert").style.display = "none"
     prevContractsRev.current = contracts
-    if (!isRendered.current && contracts.length != 0) {
-      
-      // const upload = multer({ dest: 'uploads/' })
-      // axios.post('/upload-contracts', upload.array('contracts', contracts, function (req, res, next){}))
-      // .then((res) => {
-      //   console.log(res.data)
-      // })
-      // .catch((err) => {
-      //     // inform the user
-      //     console.error(err)
-      // })
-
-      const formData = new FormData();
-
-      contracts.forEach((contract) => {
-        // Get the file object
-        // const file = contract
-        // const fs = require('fs');
-  
-        // // Specify the path to the directory where you want to save the file
-        // let data = "This is a file containing a collection of books.";
-          
-        // // Use the fs.writeFile() method to save the file
-        // fs.writeFile("books.txt", data, (err) => {
-        //   if (err)
-        //     console.log(err);
-        //   else {
-        //     console.log("File written successfully\n");
-        //     console.log("The written has the following contents:");
-        //     console.log(fs.readFileSync("books.txt", "utf8"));
-        //   }
-        // });
-
-        formData.append("contracts", contract);
-      })
-      
-      // for(let obj of formData) {
-      //   console.log(obj); // key1 = value1, then key2 = value2
-      // }
-    }
-    isRendered.current = false
   },[contracts])
+
   useEffect(() => { // run when user picked an detector to scan against their smart contracts
     if (detectorSelected) {
-      if(window.confirm("start scanning your smart contracts agains > " + detectorSelected.id + "?")){ 
-        // console.log(JSON.stringify(detectorSelected))
-        axios({
-          url: '/scan',
-          method: 'POST',
-          data: detectorSelected
+      if(window.confirm("start scanning your smart contracts agains > " + detectorSelected.id + "?")) {
+       
+        const formData = new FormData()
+        // append every files uploaded by user in contracts into formData
+        contracts.forEach((contract) => {
+          formData.append(contract.name, contract);
         })
-        .then(function (response) {
-            // your action after success
-            console.log(response);
+
+        formData.append("detector", detectorSelected.id)
+        // post contracts via the API for processing
+        axios.post('/scan_vulnerabilities', formData)
+        .then((res) => {
+          console.log(res.data)
         })
-        .catch(function (error) {
-          // your action on error success
-            console.log(error);
-        });
+        .catch((err) => {
+            // inform the user
+            console.error(err)
+        })
+
+        // To list out what are the files in the formData
+        // for(let obj of formData) {
+        //   console.log(obj); // key1 = value1, then key2 = value2
+        // }
       }
     }
   },[detectorSelected])
+
+  useEffect(() => {
+    if(percent === 100){
+      // direct user to next step if the percentage has reached 100%
+      document.getElementsByClassName("_hsN1w")[0].click()
+    }
+  },[percent])
 
   const set_scan_vulnerability = (vulnerabilityAuditorPicked) => {
     // console.log(vulnerabilityAuditorPicked)
@@ -105,40 +80,13 @@ function Audit_Process() {
   }
 
   // setup step validators, will be called before proceeding to the next step
-  function step1Validator(contracts) {
-    alert(contracts)
-    // // upload file
-    // const formData = new FormData();
-    // formData.append(
-    //     "newFile",
-    //     file,
-    //     file.name
-    // )
-    // axios.post('http://localhost:8080/upload', formData)
-    //     .then((res) => {
-    //         setFiles([...files, file])
-    //     })
-    //     .catch((err) => {
-    //         // inform the user
-    //         console.error(err)
-    //         removeFile(file.name)
-    //     });
+  const step1Validator = (contracts) => {
+    // ensure user has uploaded at least 1 contract to be scanned before proceeding to next step
+    if (contracts.length === 0){
+      alert("Please upload at least 1 contract to proceed")
+      return false
+    }
     return true;
-  }
-
-  function step2Validator() {
-    return true;
-  }
-  
-  function step3Validator() {
-    return true;
-  }
-  
-  function onFormSubmit() {
-    // handle the submit logic here
-    // This function will be executed at the last step
-    // when the submit button (next button in the previous steps) is pressed
-    alert("submited");
   }
 
   return (
@@ -147,7 +95,6 @@ function Audit_Process() {
       <div className='Step_container'>
         <StepProgressBar 
           startingStep={0} 
-          onSubmit={onFormSubmit} 
           steps={[
             {
               label: 'Upload',
@@ -155,27 +102,24 @@ function Audit_Process() {
               content: 
                 <>
                   <ContractUploader setContractsHandler={setContractsHandler} />
-                  {/* {console.log(<FileList contracts={contracts} removeFile={removeFile} />)} */}
+                  <FileList contracts={prevContractsRev.current} removeFile={removeFile} />
                 </>,
-              validator: () => step1Validator(contracts)
+              validator: () => step1Validator(prevContractsRev.current)
             },
             {
               label: 'Vulnerability',
               name: 'step 2',
-              content: <VulnerabilityPicker detectorSelected={detectorSelected} set_scan_vulnerability={set_scan_vulnerability}/>,
-              validator: step2Validator
+              content: <VulnerabilityPicker detectorSelected={detectorSelected} set_scan_vulnerability={set_scan_vulnerability}/>
             },
             {
               label: 'Scanning',
               name: 'step 3',
-              content: <Scan percentage={percentage} />,
-              validator: step3Validator
+              content: <Scan percent={percent} setPercent={() => setPercent(100)}/>
             },
             {
               label: 'Audit Report',
               name: 'step 4',
-              content: <AuditReport/>,
-              validator: step3Validator
+              content: <AuditReport/>
             }
           ]}
         />
